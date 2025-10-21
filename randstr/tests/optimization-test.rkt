1f796d24a4ecbb2f8ed21e7f07de327b0558a47d
Hugefiver <i@iruri.moe>
2025-10-21 18:57:37 +0800

    test: Add checks for generated string patterns to ensure correctness of optimized implementation

--- randstr/tests/optimization-test.rkt
+++ randstr/tests/optimization-test.rkt
@@ -1,31 +1,37 @@
 #lang racket/base
 
 (require rackunit
-         "../main.rkt")
+         "../main.rkt"
+         "./test-helpers.rkt")
+
+(define (check pattern)
+  ((make-pattern-checker pattern)))
 
 ;; Test that the optimized implementation produces the same results as expected
 
 (test-case "randstr basic functionality"
-  (let ([result (randstr "abc")])
-    (check-equal? result "abc"))
+  (check-true (check "abc"))
   
   (let ([result (randstr "[a-z]{3}")])
     (check-equal? (string-length result) 3)
+    (check-true (check "[a-z]{3}"))
     (for ([char (string->list result)])
       (check-true (char>=? char #\a))
       (check-true (char<=? char #\z))))
-
+ 
   (let ([results (randstr* "[0-9]{2}" 5)])
     (check-equal? (length results) 5)
     (for ([result results])
       (check-equal? (string-length result) 2)
+      (check-true (check "[0-9]{2}"))
       (for ([char (string->list result)])
         (check-true (char>=? char #\0))
         (check-true (char<=? char #\9))))))
 
 (test-case "character class with quantifier"
   (let ([result (randstr "[a-z]{5}")])
-    (check-equal? (string-length result) 5)))
+    (check-equal? (string-length result) 5)
+    (check-true (check "[a-z]{5}"))))
 
 ;; Run all tests
 (void)
